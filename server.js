965cb15cccadecdb9d7019aa129dc0e375ff12ce
Pixeladed <vietminhle98@gmail.com>
2014-11-01 06:29:36 +0000

    move to express

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 var fs = require('fs');
 var http = require('http');
 var port = process.env.PORT || 8000;
+var express = require('express');
 
-var server = http.createServer(function(req,res) {
-	res.writeHead(200, {'Content-type':'text/html'});
-	if (req.url == '/') {
-    	var filePath = '/index.html';
-	} else if (req.url == '') {
-	    var filePath = '/index.html';
-	} else {
-	    var filePath = req.url;
-	}
-	fs.readFile('./client'+filePath,function(error,data) {
-		if (error) {
-			res.writeHead(500, {'Content-type':'text/plain'});
-			res.end("Oh dear, something has gone horribly wrong, please reload the page"+error+req.url);
-		} else {
-			res.writeHead(200, {'Content-type':'text/html'});
-			res.end(data);
-		}
-	});
-});
-server.listen(port);
+// var server = http.createServer(function(req,res) {
+// 	res.writeHead(200, {'Content-type':'text/html'});
+// 	if (req.url == '/') {
+//     	var filePath = '/index.html';
+// 	} else if (req.url == '') {
+// 	    var filePath = '/index.html';
+// 	} else {
+// 	    var filePath = req.url;
+// 	}
+// 	fs.readFile('./client'+filePath,function(error,data) {
+// 		if (error) {
+// 			res.writeHead(500, {'Content-type':'text/plain'});
+// 			res.end("Oh dear, something has gone horribly wrong, please reload the page"+error+req.url);
+// 		} else {
+// 			res.writeHead(200, {'Content-type':'text/html'});
+// 			res.end(data);
+// 		}
+// 	});
+// });
+var app = express();
+app.use(express.static(__dirname + '/client'));
+
+var server = http.createServer(app);
+
+app.listen(port);
 console.log('LISTENING on ' + port);
 var io = require('socket.io').listen(server);
 io.configure(function() { 
