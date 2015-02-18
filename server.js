0819a1121b7f50b3ef0bc56d3802866a70609206
Pixeladed <vietminhle98@gmail.com>
2015-02-18 17:06:30 +0800

    hotjar analytic

--- server.js
+++ server.js
@@ -2,27 +2,9 @@ var fs = require('fs');
 var http = require('http');
 var port = process.env.PORT || 8000;
 var express = require('express');
-
-// var server = http.createServer(function(req,res) {
-// 	res.writeHead(200, {'Content-type':'text/html'});
-// 	if (req.url == '/') {
-//     	var filePath = '/index.html';
-// 	} else if (req.url == '') {
-// 	    var filePath = '/index.html';
-// 	} else {
-// 	    var filePath = req.url;
-// 	}
-// 	fs.readFile('./client'+filePath,function(error,data) {
-// 		if (error) {
-// 			res.writeHead(500, {'Content-type':'text/plain'});
-// 			res.end("Oh dear, something has gone horribly wrong, please reload the page"+error+req.url);
-// 		} else {
-// 			res.writeHead(200, {'Content-type':'text/html'});
-// 			res.end(data);
-// 		}
-// 	});
-// });
 var app = express();
+
+
 app.use(express.static(__dirname + '/client'));
 
 var server = http.createServer(app);
